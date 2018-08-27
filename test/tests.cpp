90bff9f290770d6cbdfd2d4800cc5ef1cc891d64
Murat9202 <40727872+Murat9202@users.noreply.github.com>
2018-08-27 13:08:13 +0400

    v2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,71 @@
 #include "gtest/gtest.h"
+#include "common.h"
+
+#include "ocean.h"
+
+
+TEST(lab3,task1_1)
+{
+    Ocean *atlantic =new Ocean;
+	
+    char ch[200] = "Test1.txt";
+		
+    bool result=atlantic->read(ch);
+
+    EXPECT_EQ(1,result);
+}
+
+
+TEST(lab3,task1_2)
+{
+    Ocean *atlantic =new Ocean;
+	
+    char ch[200] = "Test2.txt";
+		
+	
+    bool result=atlantic->read(ch);
+
+    EXPECT_EQ(1,result);
+}
+
+
+TEST(lab3,task1_3)
+{
+    Ocean *atlantic =new Ocean;
+	
+    char ch[200] = "Coral";
+	
+	
+    int result=atlantic->fill(ch, 1, 4);
+
+    EXPECT_EQ(4,result);
+}
+
+
+TEST(lab3,task1_4)
+{
+    Ocean *atlantic =new Ocean;
+	
+    char ch[200] = "Predator";
+	
+	
+    int result=atlantic->fill(ch, 1, 11);
+
+
+     
+    EXPECT_EQ(11,result);
+}
+
+
+TEST(lab3,task1_5)
+{
+    Ocean *atlantic =new Ocean;
+	
+    char ch[200] = "Время цикла питания";
+	
+	
+    int result=atlantic->fill(ch, 1, 9);
+
+    EXPECT_EQ(9,result);
+}
 
