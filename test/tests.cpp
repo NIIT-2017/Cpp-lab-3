194854847ccafd2f90f95ad182f72b68d6800331
Murat9202 <40727872+Murat9202@users.noreply.github.com>
2018-08-28 08:30:15 +0400

    v1.10

--- test/tests.cpp
+++ test/tests.cpp
@@ -67,7 +67,7 @@ TEST(lab3,ocean_5)
 {
     Ocean *atlantic =new Ocean;
 	
-    char ch[200] = "Время цикла питания";
+    char ch[200] = "feedCycle";
 	
 	
     int result=atlantic->fill(ch, 1, 9);
