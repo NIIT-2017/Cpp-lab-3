7c39f863d5a25a1ea75f97838c410ff0b16368ff
YakovDeLeon <40718860+YakovDeLeon@users.noreply.github.com>
2018-08-28 23:09:22 +0300

    Update Ocean.cpp
    Изменил реализацию _khbit()

--- src/Ocean.cpp
+++ src/Ocean.cpp
@@ -252,7 +252,7 @@ int Ocean::isEnd() // 1 - нажата Q, 2 - Нет живых объектов
 					// 0 - Всё хорошо
 {
 	
-	if (_kbhit() && toupper(cin.get()) == 'Q')
+	if (_kbhit() && toupper(std::cin.get()) == 'Q')
 	{	
 		//system("cls");
 		std::cout << "Good Bye!\n";
@@ -285,20 +285,20 @@ unsigned int Ocean::getSize()
 }
 
 int _kbhit() {
-    static const int STDIN = 0;
-    static bool initialized = false;
+static const int STDIN = 0;
+static bool initialized = false;
 
-    if (! initialized) {
-        // Use termios to turn off line buffering
-        termios term;
-        tcgetattr(STDIN, &term);
-        term.c_lflag &= ~ICANON;
-        tcsetattr(STDIN, TCSANOW, &term);
-        setbuf(stdin, NULL);
-        initialized = true;
-    }
+if (! initialized) {
+    // Use termios to turn off line buffering
+    termios term;
+    tcgetattr(STDIN, &term);
+    term.c_lflag &= ~ICANON;
+    tcsetattr(STDIN, TCSANOW, &term);
+    setbuf(stdin, NULL);
+    initialized = true;
+}
 
-    int bytesWaiting;
-    ioctl(STDIN, FIONREAD, &bytesWaiting);
-    return bytesWaiting;
+int bytesWaiting;
+ioctl(STDIN, FIONREAD, &bytesWaiting);
+return bytesWaiting;
 }
